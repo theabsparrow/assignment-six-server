afb226f247bbc6a59635f9027a0d56e0095483fe
theabsparrow <absbashar04@gmail.com>
2025-08-24 03:31:38 +0600

    auth refresh1 done

--- src/middlewire/auth.verifyToken.ts
+++ src/middlewire/auth.verifyToken.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
 import { TUSerRole } from "../module/user/user.interface";
@@ -16,15 +18,14 @@ export const verifyOtpToken = (...requiredRoles: TUSerRole[]) => {
       throw new AppError(StatusCodes.UNAUTHORIZED, "you are not authorized");
     }
     let decoded;
+
     try {
       decoded = verifyToken(token, config.jwt_refresh1_secret as string);
     } catch (err: any) {
-      throw new AppError(
-        StatusCodes.UNAUTHORIZED,
-        `you are not authorized ${err}`
-      );
+      throw new AppError(StatusCodes.UNAUTHORIZED, `otp expired `);
     }
     const { userId, userRole } = decoded as JwtPayload;
+
     const id = userId.split(" ")[0];
     const isUserExists = await User.findById(id);
     if (!isUserExists) {

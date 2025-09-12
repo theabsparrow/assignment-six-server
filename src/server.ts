08dda3afd02e474eeca5e38158bc2d3ba9a5aba7
theabsparrow <absbashar04@gmail.com>
2025-09-12 23:41:58 +0600

    commit 103

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ let server: Server;
 async function main() {
   try {
     if (!config.database_url) throw new Error("DATABASE_URL is missing!");
-    await mongoose.connect(config.database_url);
+    await mongoose.connect(process.env.DATABASE_URL as string);
     console.log("✅ MongoDB connected successfully");
     await seedSuperAdmin();
     await seedSuperAdmin();

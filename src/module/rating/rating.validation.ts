cab3d775b02b203159c62357df5cf77a7ddeef38
theabsparrow <absbashar04@gmail.com>
2025-09-15 04:29:22 +0600

    rating update code rebuild

--- src/module/rating/rating.validation.ts
+++ src/module/rating/rating.validation.ts
@@ -28,14 +28,16 @@ const updateRatingValidationSchema = z.object({
       required_error: "rating is required",
     })
     .min(1, "rating must be at least 1")
-    .max(5, "rating will be maximum 5"),
+    .max(5, "rating will be maximum 5")
+    .optional(),
   feedback: z
     .string({
       required_error: "feedback is required",
     })
     .trim()
     .min(10, { message: "feedback should be at least 10 character" })
-    .max(350, { message: "feedback can`t be more than 350 character" }),
+    .max(350, { message: "feedback can`t be more than 350 character" })
+    .optional(),
 });
 
 export const ratingValidation = {
